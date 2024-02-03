c83023104a40bd18f55d4956c570b244825a3289
Amal01111 <g_amal@live.concordia.ca>
2024-02-03 10:46:57 -0500

    change on feb 3

--- p1.java
+++ p1.java
@@ -3,6 +3,7 @@ public class p1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("this is on february 3, 2024.");
 		// TODO Auto-generated method stub
 
 	}
---
+++ p3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class p3 {
+	public static void main(String[] args) {
+		System.out.println("hello this is a github project demo");
+		System.out.println("this is latest update");
+	}
+
+}

ff540fc25b3eaa23eb7f2af49336d8ff71575960
Amal01111 <g_amal@live.concordia.ca>
2024-02-03 10:52:12 -0500

    Update p1.java

--- p1.java
+++ p1.java
@@ -4,6 +4,7 @@ public class p1 {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("this is on february 3, 2024.");
+		System.out.println("this is change number 2");
 		// TODO Auto-generated method stub
 
 	}
